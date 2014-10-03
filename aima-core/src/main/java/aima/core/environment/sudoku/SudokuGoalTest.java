26546306f01cb5dbf386826e87ffe143334d99bc
agirault <girault.al@gmail.com>
2014-10-03 18:21:29 -0400

    ENH : display Board at each GoalTest

--- aima-core/src/main/java/aima/core/environment/sudoku/SudokuGoalTest.java
+++ aima-core/src/main/java/aima/core/environment/sudoku/SudokuGoalTest.java
@@ -7,6 +7,9 @@ public class SudokuGoalTest implements GoalTest{
 	@Override
 	public boolean isGoalState(Object state) {
 		SudokuBoard board = (SudokuBoard) state;
+		
+		System.out.println(board);
+		
 		if (board.isFilled()){
 			return board.isAllConstraintsSatisfied();
 		}
